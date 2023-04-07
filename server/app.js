7ff9cd16abd513dd9e562c4b392475796d98a8c0
deepalisonavane <deepalisonavane999@gmail.com>
2023-04-07 15:17:43 +0530

    connection made

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+dotenv.config({path:'./config.env'});
+require('./db/conn')
+
+app.use(require('./routes/auth'))
+const User =require('./models/userschema');
+const PORT = process.env.PORT;
+
+const middleware = (req,res,next)=>{
+    console.log("middleware");
+    next();
+}
+
+
+app.get('/', (req,res)=>{
+    res.send("hellloo");
+
+});
+
+app.get('/signin',middleware, (req,res)=>{
+    res.send("signinn");
+
+});
+
+app.get('/signup', (req,res)=>{
+    res.send("signup");
+
+});
+app.listen(PORT,()=>{
+console.log(`server running on ${PORT}`);
+})
+
